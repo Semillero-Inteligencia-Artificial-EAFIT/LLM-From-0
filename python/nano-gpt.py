780c0712030ad95a7202b92fd7be501e3a849d3b
dasjero98772 <jero98772@gmail.com>
2025-08-05 11:54:12 -0500

    update code

--- python/nano-gpt.py
+++ python/nano-gpt.py
@@ -1,7 +1,40 @@
+import torch
+
 def read_txt_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     return content
 
-
 text = read_txt_file("example.txt")
+chars = sorted(set(text))
+vocab_size = len(chars)
+
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+
+def encode(s):
+    return [stoi[c] for c in s]
+
+def decode(l):
+    return ''.join([itos[i] for i in l])
+
+#encode=encode("hello")
+#decode=decode(encode)
+#print(decode)
+
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000])
+
+n = int(len(data) * 0.9)
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8
+
+x = train_data[:block_size]
+y = train_data[1:block_size + 1]
+for t in range(block_size):
+    context = x[:t + 1]
+    target = y[t]
+    print(f"when input is {context} the target: {target}")
